4be94b73d643eab01538886f589286e434a6a8ae
Bruce Inazawa <42726157+brucezika1@users.noreply.github.com>
2022-05-25 17:00:49 -0300

    Update index.js

--- blink/www/js/index.js
+++ blink/www/js/index.js
@@ -1,29 +1,60 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var app = {
+        
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    onDeviceReady: function() {
+        document.getElementById("btnInserir").addEventListener("click",app.inserir);
+        document.getElementById("btnListar").addEventListener("click",app.listar);
+        this.receivedEvent('deviceready');
+    },
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+    receivedEvent: function(id) {
+        banco = window.sqlitePlugin.openDatabase({
+            name: 'Konoha',
+            location: 'default',            
+            androidDatabaseProvider: 'system'
+        });
+
+        banco.transaction(function(tx) {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (name, senha)');
+        }, function(error) {
+            console.log('Transaction ERROR: ' + error.message);
+        }, function() {
+            alert('Banco e Tabela criados com sucesso!');
+        });
+    },
+
+    inserir: function(){
+        let name = document.getElementById("txtLogin").value;
+        let senha = document.getElementById("txtPassword").value;
+
+        banco.transaction(function(tx) {
+            tx.executeSql('INSERT INTO usuarios VALUES (?,?)', [name, senha]);
+        }, function(error) {
+            alert('Erro durante a transacao com o banco: ' + error.message);
+        }, function() {
+            alert('Insercao realizada com sucesso!');
+        });
+    },
+    
+    listar: function(){
+        banco.executeSql(
+            'SELECT name AS txtLogin, pass AS txtPassword FROM usuarios', [], function(rs) {
+                alert(JSON.stringify(rs));
+                alert(rs.rows.length);
+                let i = 0;
+                for(i = 0; i < rs.rows.length; i++){
+                    alert("item "+i);
+                    let recordItem = rs.rows.item(i);
+                    alert(JSON.stringify(recordItem));
+                }                
+        }, function(error) {
+            alert('Erro no SELECT: ' + error.message);
+        });
+    }
+};
+
+app.initialize();
