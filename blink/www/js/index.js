56128f88d24ed4e6dd4edbc8508260426b76cd48
Bruce Inazawa <42726157+brucezika1@users.noreply.github.com>
2022-06-15 20:17:53 -0300

    Update index.js

--- blink/www/js/index.js
+++ blink/www/js/index.js
@@ -1,59 +1,55 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
 var app = {
-        
 
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
     onDeviceReady: function() {
-        document.getElementById("btnInserir").addEventListener("click",app.inserir);
-        document.getElementById("btnListar").addEventListener("click",app.listar);
+        alert("onDeviceReady: function() {...");
         this.receivedEvent('deviceready');
+        navigator.geolocation.getCurrentPosition(
+            this.onGeolocationSuccess,
+            this.onGeolocationError,
+            { timeout: 120000 });
     },
 
+    // Update DOM on a Received Event
     receivedEvent: function(id) {
-        banco = window.sqlitePlugin.openDatabase({
-            name: 'Konoha',
-            location: 'default',            
-            androidDatabaseProvider: 'system'
-        });
-
-        banco.transaction(function(tx) {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS usuarios (name, senha)');
-        }, function(error) {
-            console.log('Transaction ERROR: ' + error.message);
-        }, function() {
-            alert('Banco e Tabela criados com sucesso!');
-        });
     },
 
-    inserir: function(){
-        let name = document.getElementById("txtLogin").value;
-        let senha = document.getElementById("txtPassword").value;
+    onGeolocationSuccess: function(position) {
+        alert("onGeolocationSuccess: function(position) {...");
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
 
-        banco.transaction(function(tx) {
-            tx.executeSql('INSERT INTO usuarios VALUES (?,?)', [name, senha]);
-        }, function(error) {
-            alert('Erro durante a transacao com o banco: ' + error.message);
-        }, function() {
-            alert('Insercao realizada com sucesso!');
-        });
+        document.getElementById("latitude").value = position.coords.latitude;
+        document.getElementById("longitude").value = position.coords.longitude;
+        document.getElementById("altitude").value = position.coords.altitude;
+        document.getElementById("accuary").value = position.coords.accuary;
+        document.getElementById("altitudeaccuary").value = position.coords.altitudeAccuary;
+        document.getElementById("heading").value = position.coords.heading;
+        document.getElementById("speed").value = position.coords.speed;
+        document.getElementById("timestamp").value = position.coords.timestamp;
     },
-    
-    listar: function(){
-        banco.executeSql(
-            'SELECT name AS txtLogin, pass AS txtPassword FROM usuarios', [], function(rs) {
-                alert(JSON.stringify(rs));
-                alert(rs.rows.length);
-                let i = 0;
-                for(i = 0; i < rs.rows.length; i++){
-                    alert("item "+i);
-                    let recordItem = rs.rows.item(i);
-                    alert(JSON.stringify(recordItem));
-                }                
-        }, function(error) {
-            alert('Erro no SELECT: ' + error.message);
-        });
+
+    onGeolocationError: function(err) {
+        alert("onGeolocationError:function(position) {...");
+        alert(err.code +" "+err.message);
     }
 };
 
